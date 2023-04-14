d29f1ef62d5936c70ac8d7b19ccfb848505f6894
KristianColville1 <kristiancolville1@gmail.com>
2023-04-14 20:38:25 +0100

    Initial commit

---
+++ JavaScript/destructuring.js
@@ -0,0 +1,22 @@
+/**
+ * Destructuring
+ * 
+ * Allows you to easily extract elements or object properties and
+ * store them in variables.
+ * 
+ * Array Destructuring & Object Destructuring.
+ * 
+ * Next Gen examples of JavaScript.
+ */
+
+// Arrays
+let [greet, user] = ['Hello', 'Kristian']
+
+console.log(greet) // Hello
+console.log(user) // Kristian
+
+// Objects
+let { userName, age } = { userName: 'Kristian', age: 28 }
+
+console.log(userName) // Kristian
+console.log(age) // undefined
